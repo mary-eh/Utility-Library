e61c6f3f138d055bcfb7e973d7fbbc44e382fd81
Hooman <H00man86j@gmail.com>
2026-07-06 11:40:03 +0330

    Add reverseNumber function

--- functions.c
+++ functions.c
@@ -1,1 +1,21 @@
+#include "functions.h"
 
+int reverseNumber(int n)
+{
+    int reversed = 0;
+    int sign = 1;
+
+    if (n < 0)
+    {
+        sign = -1;
+        n = -n;
+    }
+
+    while (n != 0)
+    {
+        reversed = reversed * 10 + (n % 10);
+        n /= 10;
+    }
+
+    return reversed * sign;
+}
